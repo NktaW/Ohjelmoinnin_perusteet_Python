360174dd38a585512dc1610ba3252eeac4117d6e
Nikita Waara <nikita.waara@hotmail.com>
2022-10-28 17:20:13 +0300

    adding program

---
+++ GiveInfo/annaNimesiJaSyntymaAika.py
@@ -0,0 +1,7 @@
+import random
+
+nimi = input("Anna nimesi: ")
+syntymavuosi = int(input("Anna syntymavuosi: "))
+summa = 2021 - syntymavuosi
+print("Moi, " + nimi+"!")
+print("Olet ", summa, " vuotta vanha vuoden 2021 lopussa.")
